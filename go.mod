d5ac77b5665d69e2303c5dace0c42286ba4dbb07
jackhydez <pash.ok92@yandex.ru>
2023-09-10 00:47:44 +0300

    add v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jackhydez/pakagetest
+module github.com/jackhydez/pakagetest/v2
 
 go 1.18
--- sum.go
+++ sum.go
@@ -1,5 +1,5 @@
 package packagetest
 
-func Sum(x, y int) int {
-	return x + y
+func Sum(x, y, z int) int {
+	return x + y + z
 }
